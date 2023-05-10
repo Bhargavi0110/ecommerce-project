90f454aa4dc9f57f1b011da1200bf2516f5095d3
Bhargavi0110 <105622080+Bhargavi0110@users.noreply.github.com>
2023-05-10 11:40:38 +0530

    added a logg message

--- src/main/java/com/example/ecommerce/services/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce/services/ProductServiceImpl.java
@@ -109,7 +109,7 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductImages saveImage(MultipartFile file) throws IOException {
-        logger.info("In saveImage method");
+        logger.info("In saveImage method of prooductserviceImpl class");
         ProductImages productImages=new ProductImages();
         productImages.setName(file.getOriginalFilename());
         productImages.setImageData(file.getBytes());
